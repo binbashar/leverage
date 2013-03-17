dacbac922d932e152de9ff1d1c40379232d526f0
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-03-18 03:25:52 +0530

    changed web link

--- pynt/__init__.py
+++ pynt/__init__.py
@@ -4,7 +4,7 @@ Lightweight Python Build Tool
 
 __version__ = "0.7.1"
 __license__ = "MIT License"
-__contact__ = "https://github.com/rags/pynt"
+__contact__ = "http://rags.github.com/pynt/"
 from ._pynt import task, main
 
 __all__ = ["task",  "main"]
