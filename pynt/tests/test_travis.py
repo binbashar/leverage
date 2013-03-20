985d5aac9827fd1b07fe19338273d97b3a3e99d1
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-03-20 20:54:36 +0530

    getting it to fail

--- pynt/tests/test_travis.py
+++ pynt/tests/test_travis.py
@@ -3,4 +3,4 @@ from pynt import _pynt, main
 import pytest
 def test_foo():
     with pytest.raises(SystemExit):
-          _pynt._create_parser().parse_args("[--file]") # this line should throw SystemExit
+          _pynt._create_parser().parse_args(["--file"]) # this line should throw SystemExit
