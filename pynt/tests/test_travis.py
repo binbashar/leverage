78d38500b2fef5e6ef96283aef30fb7e8a584f8d
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-03-20 20:51:35 +0530

    passes strangeley

--- pynt/tests/test_travis.py
+++ pynt/tests/test_travis.py
@@ -1,8 +1,6 @@
+from pynt import _pynt, main
+
 import pytest
 def test_foo():
     with pytest.raises(SystemExit):
-          import argparse
-          parser =  argparse.ArgumentParser()
-          parser.add_argument('-f', '--file', help= "Build file",
-                            metavar= "file", default= "build.py")
-          parser.parse_args("[--file]") # this line should throw SystemExit
+          _pynt._create_parser().parse_args("[--file]") # this line should throw SystemExit
