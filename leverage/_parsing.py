aed429fca8c7f630aad0c1a7d7973f42445091c5
Angelo Fenoglio <angelo.fenoglio@binbash.com.ar>
2021-04-14 09:44:13 -0300

    General refactor of Leverage

---
+++ leverage/_parsing.py
@@ -0,0 +1,81 @@
+"""
+    Command line arguments and tasks arguments parsing utilities.
+"""
+from operator import attrgetter
+from argparse import ArgumentParser
+
+
+class InvalidArgumentOrderError(RuntimeError):
+    pass
+
+
+class DuplicateKeywordArgumentError(RuntimeError):
+    pass
+
+
+def _get_argument_parser():
+    """ Create the required argument parser.
+
+    Returns:
+        argparse.ArgumentParser: argument parser
+    """
+    parser = ArgumentParser(add_help=False)
+
+    parser.add_argument("tasks",
+                        nargs="*",
+                        metavar="task",
+                        help="Perform specied task(s) and all of its dependencies")
+    parser.add_argument("-l", "--list-tasks",
+                        action="store_true",
+                        help="List available tasks")
+    parser.add_argument("-v", "--version",
+                        action="store_true",
+                        help="Display tool version")
+    parser.add_argument("-f", "--file",
+                        metavar="file",
+                        default="build.py",
+                        help="Name of the build file containing the tasks definitions,"
+                             " if left unspecified defaults to `build.py`")
+    parser.add_argument("-h", "--help",
+                        action='store_true',
+                        help="Print help information")
+
+    return parser
+
+def _parse_args(arguments):
+    """ Parse the arguments for a task and return args and kwargs appropriately
+
+    Args:
+        arguments (str): Arguments to parse.
+
+    Raises:
+        InvalidArgumentOrderError: When a positional argument is given after keyworded
+            arguments are already been specified.
+        DuplicateKeywordArgumentError: When a keyworded argument is specified more than once.
+
+    Returns:
+        list, dict: Args, and kwargs present in the input string.
+    """
+    args = []
+    kwargs = {}
+
+    if arguments is None:
+        return args, kwargs
+
+    arguments = arguments.split(",")
+
+    for argument in arguments:
+        if "=" not in argument:
+            if kwargs:
+                raise InvalidArgumentOrderError(f"Positional argument `{argument}` from task `{{task}}` cannot follow a keyword argument.")
+
+            args.append(argument)
+
+        else:
+            key, value = [part.strip() for part in argument.split("=")]
+            if key in kwargs:
+                raise DuplicateKeywordArgumentError(f"Duplicated keyword argument `{key}` in task `{{task}}`.")
+
+            kwargs[key] = value
+
+    return args, kwargs
