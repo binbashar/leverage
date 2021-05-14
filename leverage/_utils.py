2f5a5919c126aa28a465f6a6428d2f4d50eea538
Angelo Fenoglio <angelofenoglio@gmail.com>
2021-05-14 16:22:10 -0300

    Move _list_tasks to its own module

---
+++ leverage/_utils.py
@@ -0,0 +1,59 @@
+"""
+    Tasks displaying utilities.
+"""
+from operator import attrgetter
+
+import click
+
+from leverage import __version__
+
+
+_CREDIT_LINE = f"Powered by Leverage {__version__}"
+_IGNORED = "Ignored"
+_DEFAULT = "Default"
+
+
+def _list_tasks(module):
+    """ Print all non-private tasks in a neat table-like format.
+    Indicates whether the task is ignored and/or if it is the default one.
+
+    Args:
+        module (dict): Dictionary containing all tasks and the module name
+    """
+    # Header
+    click.echo(f"Tasks in build file `{module['name']}`:\n")
+
+    visible_tasks = [task for task in module["tasks"] if not task.is_private]
+
+    if visible_tasks:
+        tasks_grid = []
+
+        for task in sorted(visible_tasks, key=attrgetter("name")):
+            # Form the attrs column values
+            task_attrs = []
+            if task == module["__DEFAULT__"]:
+                task_attrs.append(_DEFAULT)
+            if task.is_ignored:
+                task_attrs.append(_IGNORED)
+            task_attrs = f"[{','.join(task_attrs)}]" if task_attrs else ""
+            # Split multiline docstrings to be able to handle them as a column
+            doc_lines = task.doc.splitlines()
+
+            tasks_grid.append((task.name, task_attrs, doc_lines))
+
+        name_column_width = max(len(name) for name, _, _ in tasks_grid)
+        attr_column_width = max(len(attr) for _, attr, _ in tasks_grid)
+
+        # Body
+        for name, attr, doc in tasks_grid:
+
+            click.echo(f"  {name:<{name_column_width}}  {attr: ^{attr_column_width}}\t{doc[0]}")
+            # Print the remaining lines of the dosctring with the correct indentation
+            for doc_line in doc[1:]:
+                click.echo(f"    {'': <{name_column_width + attr_column_width}}\t{doc_line}")
+
+    else:
+        click.echo("  --")
+
+    # Footer
+    click.echo(f"\n{_CREDIT_LINE}")
--- leverage/leverage.py
+++ leverage/leverage.py
@@ -229,48 +229,6 @@ def _print_version():
     _logger.info(f"leverage {__version__}")
     sys.exit(0)
 
-def _print_tasks(module):
-    """ Print all non-private tasks in a neat table-like format.
-    Indicates whether the task is ignored and/or if it is the default one.
-
-    Args:
-        module (dict): Dictionary containing all tasks and the module name
-    """
-    IGNORED = "Ignored"
-    DEFAULT = "Default"
-
-    # Header
-    print(f"Tasks in build file `{module['name']}`:\n")
-
-    visible_tasks = [task for task in module["tasks"] if not task.is_private]
-
-    if visible_tasks:
-        tasks_grid = []
-
-        for task in sorted(visible_tasks, key=attrgetter("name")):
-            # Form the attrs column values
-            task_attrs = []
-            if task == module["__DEFAULT__"]:
-                task_attrs.append(DEFAULT)
-            if task.is_ignored:
-                task_attrs.append(IGNORED)
-            task_attrs = f"[{','.join(task_attrs)}]" if task_attrs else ""
-
-            tasks_grid.append((task.name, task_attrs, task.doc))
-
-        name_column_width = max(len(name) for name, _, _ in tasks_grid)
-        attr_column_width = max(len(attr) for _, attr, _ in tasks_grid)
-
-        # Body
-        for name, attr, doc in tasks_grid:
-            print(f"  {name:<{name_column_width}}  {attr: ^{attr_column_width}}\t{doc}")
-    
-    else:
-        print("  --")
-
-    # Footer
-    print(f"\n{_CREDIT_LINE}")
-
 def _terminate(error_message):
     """ Print error message and terminate program.
 
