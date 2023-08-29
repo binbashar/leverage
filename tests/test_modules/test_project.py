b0e0ed8c36c7f6a55b97090b318e916a4b93a3a5
Francisco Rivera <franmrivera@gmail.com>
2023-08-29 14:42:57 -0300

    lower case letters, numbers and hypens, 25 chars max

--- tests/test_modules/test_project.py
+++ tests/test_modules/test_project.py
@@ -6,7 +6,9 @@ from leverage.modules.project import validate_config
 
 def test_validate_config_happy_path():
     assert validate_config({"project_name": "fine"})
-    assert validate_config({"project_name": "fine_with_underscores"})
+    assert validate_config({"project_name": "fine123"})
+    assert validate_config({"project_name": "123fine"})
+    assert validate_config({"project_name": "hyphens-are-allowed"})
 
 
 def test_validate_config_errors(muted_click_context):
@@ -14,4 +16,10 @@ def test_validate_config_errors(muted_click_context):
         validate_config({"project_name": "with spaces"})
 
     with pytest.raises(ExitError, match="Project name is not valid"):
-        validate_config({"project_name": "with-hyphen"})
+        validate_config({"project_name": "underscores_not_allowed"})
+
+    with pytest.raises(ExitError, match="Project name is not valid"):
+        validate_config({"project_name": "not-alph@-characters!"})
+
+    with pytest.raises(ExitError, match="Project name is not valid"):
+        validate_config({"project_name": "loooooooooooooooooooooooooooooooooooooooooooooooooooooong"})
