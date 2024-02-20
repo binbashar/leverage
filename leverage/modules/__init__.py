8055a71681352980a7876731198adaf8a3fce414
Francisco Rivera <franmrivera@gmail.com>
2024-02-20 08:45:30 -0300

    [BB-242] _load_configs_for_credentials test (#245)
    * _load_configs_for_credentials test
    
    * coveralls integration
    
    * fix indentation
    
    * trying xml format
    
    * skip docker image for unit tests
    
    * missing quote

--- leverage/modules/__init__.py
+++ leverage/modules/__init__.py
@@ -1,7 +1,6 @@
 from .run import run
 from .project import project
 from .terraform import terraform
-from .credentials import credentials
 from .tfautomv import tfautomv
 from .kubectl import kubectl
 from .shell import shell
