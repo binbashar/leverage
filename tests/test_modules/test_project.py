5c81cf1cca8b3aadfca2c39b3b3f93d54f26ee97
Francisco Rivera <franmrivera@gmail.com>
2024-03-10 11:11:46 -0300

    project short name validation

--- tests/test_modules/test_project.py
+++ tests/test_modules/test_project.py
@@ -4,22 +4,47 @@ from leverage._utils import ExitError
 from leverage.modules.project import validate_config
 
 
-def test_validate_config_happy_path():
-    assert validate_config({"project_name": "fine"})
-    assert validate_config({"project_name": "fine123"})
-    assert validate_config({"project_name": "123fine"})
-    assert validate_config({"project_name": "hyphens-are-allowed"})
+@pytest.mark.parametrize(
+    "project_name,short_name",
+    [
+        # different valid project names
+        ("fine", "ok"),
+        ("fine123", "ok"),
+        ("123fine", "ok"),
+        ("hyphens-are-allowed", "ok"),
+        # different valid short project names
+        ("fine", "foo"),
+        ("fine", "123"),
+        ("fine", "max3"),
+    ],
+)
+def test_validate_config_happy_path(project_name, short_name):
+    assert validate_config({"project_name": project_name, "short_name": short_name})
 
 
-def test_validate_config_errors(muted_click_context):
+@pytest.mark.parametrize(
+    "invalid_name",
+    [
+        "with spaces",
+        "underscores_not_allowed",
+        "not-alph@-characters!",
+        "loooooooooooooooooooooooooooooooooooooooooooooooooooooong",
+    ],
+)
+def test_validate_config_project_name_errors(muted_click_context, invalid_name):
     with pytest.raises(ExitError, match="Project name is not valid"):
-        validate_config({"project_name": "with spaces"})
+        validate_config({"project_name": invalid_name, "short_name": "ok"})
 
-    with pytest.raises(ExitError, match="Project name is not valid"):
-        validate_config({"project_name": "underscores_not_allowed"})
 
-    with pytest.raises(ExitError, match="Project name is not valid"):
-        validate_config({"project_name": "not-alph@-characters!"})
-
-    with pytest.raises(ExitError, match="Project name is not valid"):
-        validate_config({"project_name": "loooooooooooooooooooooooooooooooooooooooooooooooooooooong"})
+@pytest.mark.parametrize(
+    "invalid_name",
+    [
+        "longerthan4",
+        "1",
+        "@-!#",
+        "",
+    ],
+)
+def test_validate_config_short_project_name_errors(muted_click_context, invalid_name):
+    with pytest.raises(ExitError, match="Project short name is not valid"):
+        validate_config({"project_name": "test", "short_name": invalid_name})
