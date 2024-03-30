11b02763d8fbba7bc11a2dc005313c5a14c3cb22
Francisco Rivera <franmrivera@gmail.com>
2024-03-30 12:11:13 -0600

    splitting test_configure_accounts_profiles test + comments

--- tests/test_modules/test_credentials.py
+++ tests/test_modules/test_credentials.py
@@ -80,13 +80,12 @@ def test_load_configs_for_credentials(with_click_context):
     }
 
 
-@pytest.mark.parametrize("mfa_device", [False, True])
 @mock.patch("leverage.modules.credentials._get_mfa_serial", new=Mock(return_value="mfa123"))
 @mock.patch("leverage.modules.credentials._backup_file")
-def test_configure_accounts_profiles(mocked_backup, mfa_device, muted_click_context):
+def test_configure_accounts_profiles(mocked_backup, muted_click_context):
     """
     Test that the expected jsons for the aws credentials are generated as expected.
-    Cover both mfa and no-mfa cases.
+    No-mfa case.
     """
     with mock.patch("leverage.modules.credentials.configure_profile") as mocked_config:
         configure_accounts_profiles(
@@ -94,7 +93,7 @@ def test_configure_accounts_profiles(mocked_backup, mfa_device, muted_click_cont
             "us-test-1",
             {"acc1": "12345", "out-of-project-acc": "67890"},
             [{"name": "acc1"}],
-            mfa_device,
+            fetch_mfa_device=False,
         )
 
     # make sure we did a backup with the old credentials
@@ -109,8 +108,40 @@ def test_configure_accounts_profiles(mocked_backup, mfa_device, muted_click_cont
         "role_arn": f"arn:aws:iam::12345:role/OrganizationAccountAccessRole",
         "source_profile": "test-management",
     }
-    if mfa_device:
-        expected["mfa_serial"] = "mfa123"
+
+    assert mocked_config.call_args_list[0][0][1] == expected
+
+
+@pytest.mark.parametrize("mfa_device", [False, True])
+@mock.patch("leverage.modules.credentials._get_mfa_serial", new=Mock(return_value="mfa123"))
+@mock.patch("leverage.modules.credentials._backup_file")
+def test_configure_accounts_profiles_mfa(mocked_backup, mfa_device, muted_click_context):
+    """
+    Test that the expected jsons for the aws credentials are generated as expected.
+    Mfa case.
+    """
+    with mock.patch("leverage.modules.credentials.configure_profile") as mocked_config:
+        configure_accounts_profiles(
+            "test-management",
+            "us-test-1",
+            {"acc1": "12345", "out-of-project-acc": "67890"},
+            [{"name": "acc1"}],
+            fetch_mfa_device=True,
+        )
+
+    # make sure we did a backup with the old credentials
+    assert mocked_backup.called_once
+
+    # only 1 call since "out-of-project-acc" should be avoided
+    assert mocked_config.call_count == 1
+    assert mocked_config.call_args_list[0][0][0] == "test-acc1-oaar"
+    expected = {
+        "output": "json",
+        "region": "us-test-1",
+        "role_arn": f"arn:aws:iam::12345:role/OrganizationAccountAccessRole",
+        "source_profile": "test-management",
+        "mfa_serial": "mfa123",
+    }
 
     assert mocked_config.call_args_list[0][0][1] == expected
 
@@ -132,6 +163,10 @@ ACCESSKEYXXXXXXXXXXX,secretkeyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 """,
 )
 def test_extract_credentials(mocked_open):
+    """
+    Test that the access and secret keys are extracted and validated correctly
+    from the credentials.csv file provided by AWS.
+    """
     assert _extract_credentials(Path("credentials.csv")) == (
         "ACCESSKEYXXXXXXXXXXX",
         "secretkeyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
@@ -139,18 +174,27 @@ def test_extract_credentials(mocked_open):
 
 
 def test_get_organization_accounts():
+    """
+    Test that the list of accounts of an organization are queried and returned in a {acc name:  acc id} dict.
+    """
     mocked_aws_cli.exec = Mock(return_value=(0, '{"Accounts": [{"Name": "test-acc1", "Id": "12345"}]}'))
     with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
         assert _get_organization_accounts("foo", "bar") == {"test-acc1": "12345"}
 
 
 def test_get_organization_accounts_error():
+    """
+    Test that, if getting the list of accounts fails for some reason, we return an empty dict.
+    """
     mocked_aws_cli.exec = Mock(return_value=(1, "BAD"))
     with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
         assert _get_organization_accounts("foo", "bar") == {}
 
 
 def test_get_mfa_serial():
+    """
+    Test that we fetch the mfa devices from the profile and return the serial number of the first one that is valid.
+    """
     mocked_aws_cli.exec = Mock(
         return_value=(0, '{"MFADevices": [{"SerialNumber": "arn:aws:iam::123456789012:mfa/testuser"}]}')
     )
@@ -159,16 +203,13 @@ def test_get_mfa_serial():
 
 
 def test_get_mfa_serial_error(muted_click_context):
+    """
+    Test that, if fetching mfa devices fails, we return a user-friendly error.
+    """
     mocked_aws_cli.exec = Mock(return_value=(1, "BAD"))
     with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
         with pytest.raises(ExitError, match="AWS CLI error: BAD"):
             _get_mfa_serial("foo")
-
-
-def test_profile_is_configured():
-    mocked_aws_cli.exec = Mock(return_value=(0, "OK"))
-    with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
-        assert _profile_is_configured("foo")
 
 
 def test_credentials_are_valid():
@@ -188,15 +229,6 @@ def test_get_management_account_id_error(with_click_context):
     with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
         with pytest.raises(ExitError, match="AWS CLI error: BAD"):
             _get_management_account_id("foo")
-
-
-def test_backup_file():
-    with mock.patch("leverage.modules.credentials.AWSCLI", mocked_aws_cli):
-        _backup_file("config")
-
-    assert (
-        mocked_aws_cli.system_exec.call_args_list[0][0][0] == "sh -c 'cp $AWS_CONFIG_FILE \"${AWS_CONFIG_FILE}.bkp\"'"
-    )
 
 
 def test_configure_credentials(with_click_context, propagate_logs, caplog):
