83b29758ef2bb4e0561eaf17b14723e4dda64762
Francisco Rivera <franmrivera@gmail.com>
2025-03-05 12:09:48 +0100

    version validation

--- leverage/leverage.py
+++ leverage/leverage.py
@@ -1,12 +1,12 @@
 """
     Binbash Leverage Command-line tool.
 """
+from packaging.version import Version
 
 import click
 
-from leverage import __version__
+from leverage import __version__, conf
 from leverage._internals import pass_state
-
 from leverage.modules.aws import aws
 from leverage.modules.credentials import credentials
 from leverage.modules import run, project, terraform, tfautomv, kubectl, shell
@@ -25,6 +25,11 @@ def leverage(context, state, verbose):
         # leverage called with no subcommand
         click.echo(context.get_help())
 
+    # if there is a version restriction set, make sure we satisfy it
+    config = conf.load()
+    minimum_version = config.get('LEVERAGE_CLI_VERSION')
+    if minimum_version and Version(__version__) < Version(minimum_version):
+        click.echo(f"\033[91mWARNING\033[0m\tYour current version ({__version__}) is lower than the required minimum ({minimum_version}).")
 
 # Add modules to leverage
 leverage.add_command(run)
