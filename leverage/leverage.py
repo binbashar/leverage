1addb9d4c181ff1afc15af47ba3d0a150fda16d3
Francisco Rivera <franmrivera@gmail.com>
2025-03-05 12:11:01 +0100

    black

--- leverage/leverage.py
+++ leverage/leverage.py
@@ -1,6 +1,7 @@
 """
     Binbash Leverage Command-line tool.
 """
+
 from packaging.version import Version
 
 import click
@@ -27,9 +28,12 @@ def leverage(context, state, verbose):
 
     # if there is a version restriction set, make sure we satisfy it
     config = conf.load()
-    minimum_version = config.get('LEVERAGE_CLI_VERSION')
+    minimum_version = config.get("LEVERAGE_CLI_VERSION")
     if minimum_version and Version(__version__) < Version(minimum_version):
-        click.echo(f"\033[91mWARNING\033[0m\tYour current version ({__version__}) is lower than the required minimum ({minimum_version}).")
+        click.echo(
+            f"\033[91mWARNING\033[0m\tYour current version ({__version__}) is lower than the required minimum ({minimum_version})."
+        )
+
 
 # Add modules to leverage
 leverage.add_command(run)
