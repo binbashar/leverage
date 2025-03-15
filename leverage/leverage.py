2ffe5a2de05b644fc66bef5ee869686bde3ce942
Francisco Rivera <franmrivera@gmail.com>
2025-03-15 10:25:14 +0100

    fix comment

--- leverage/leverage.py
+++ leverage/leverage.py
@@ -39,7 +39,7 @@ def leverage(context, state, verbose):
     if not (current_values := config.get("TERRAFORM_IMAGE_TAG")):
         # at some points of the project (the init), the config file is not created yet
         return
-    # validate both CLI and TF versions
+    # validate both TOOLBOX and TF versions
     for key, current in zip(MINIMUM_VERSIONS, current_values.split("-")):
         if Version(current) < Version(MINIMUM_VERSIONS[key]):
             rich.print(
