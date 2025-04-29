4059594df9b5ab77bc4bfed2f6140108b53b8296
Angelo Fenoglio <angelo.fenoglio@binbash.com.ar>
2025-04-29 10:47:11 -0300

    Create tofu command

--- leverage/modules/__init__.py
+++ leverage/modules/__init__.py
@@ -1,6 +1,6 @@
 from .run import run
 from .project import project
-from .terraform import terraform
+from .terraform import tofu, terraform
 from .tfautomv import tfautomv
 from .kubectl import kubectl
 from .shell import shell
